0f208ccddf3accba4c6ea45956d78ca0d6e4ec84
jolros <jolros@users.noreply.github.com>
2025-11-29 18:25:42 +0000

    deploy: a1152ffe5c83253862252e0fdbe9314dc9c6d9ab

--- trait.impl/core/convert/trait.From.js
+++ trait.impl/core/convert/trait.From.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["planchet",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.1/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;Error&gt; for <a class=\"enum\" href=\"planchet/enum.Error.html\" title=\"enum planchet::Error\">Error</a>"]]]]);
+    var implementors = Object.fromEntries([["planchet",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.1/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;Error&gt; for <a class=\"enum\" href=\"planchet/enum.Error.html\" title=\"enum planchet::Error\">Error</a>"]]],["planchet_cli",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.1/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;SearchTypeResult&gt; for <a class=\"struct\" href=\"planchet_cli/struct.TypeResult.html\" title=\"struct planchet_cli::TypeResult\">TypeResult</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[268]}
+//{"start":57,"fragment_lengths":[268,313]}
--- trait.impl/tabled/tabled/trait.Tabled.js
+++ trait.impl/tabled/tabled/trait.Tabled.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["planchet_cli",[["impl Tabled for <a class=\"struct\" href=\"planchet_cli/struct.IssuerSummary.html\" title=\"struct planchet_cli::IssuerSummary\">IssuerSummary</a>"]]]]);
+    var implementors = Object.fromEntries([["planchet_cli",[["impl Tabled for <a class=\"struct\" href=\"planchet_cli/struct.IssuerSummary.html\" title=\"struct planchet_cli::IssuerSummary\">IssuerSummary</a>"],["impl Tabled for <a class=\"struct\" href=\"planchet_cli/struct.TypeResult.html\" title=\"struct planchet_cli::TypeResult\">TypeResult</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[169]}
+//{"start":57,"fragment_lengths":[311]}
